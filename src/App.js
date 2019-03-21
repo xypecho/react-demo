a8114f9d89a7d006df8c11552e93eda9b030d078
xypecho <xypecho@163.com>
2019-03-21 14:12:35 +0800

    项目雏形

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Button } from 'antd';
 import './App.css';
-import './test.less';
 
 class App extends Component {
   render() {
