22d340799593196e23a329903bfe02f54eb5a9df
xypecho <xypecho@163.com>
2019-03-25 23:49:24 +0800

    完成导航栏

--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -6,8 +6,28 @@ import './index.less';
 const SubMenu = Menu.SubMenu;
 
 export default class NavLeft extends React.Component {
-    componentDidMount() {
+    state = {
+        openKeys: [0],
+        rootSubmenuKeys: []
+    };
+    componentWillMount() {
         console.log(menuConfig);
+        menuConfig.map((item, k) => {
+            this.state.rootSubmenuKeys.push(k);
+        })
+    }
+    onOpenChange = (openKeys) => {
+        console.log(openKeys);
+        console.log(this.state.rootSubmenuKeys);
+        // const latestOpenKey = openKeys.find(key => this.state.openKeys.indexOf(key) === -1);
+        // console.log(latestOpenKey);
+        // if (this.rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
+        //     this.setState({ openKeys });
+        // } else {
+        //     this.setState({
+        //         openKeys: latestOpenKey ? [latestOpenKey] : [],
+        //     });
+        // }
     }
     render() {
         return (
@@ -17,37 +37,32 @@ export default class NavLeft extends React.Component {
                     <h1>后台管理系统</h1>
                 </div>
                 <Menu
-                    defaultSelectedKeys={['1']}
-                    defaultOpenKeys={['sub1']}
+                    defaultSelectedKeys={this.state.openKeys}
+                    defaultOpenKeys={this.state.openKeys}
                     mode="inline"
                     theme="dark"
-                >
-                    <Menu.Item key="1">
-                        <Icon type="pie-chart" />
-                        <span>工作台</span>
-                    </Menu.Item>
-                    <Menu.Item key="2">
-                        <Icon type="desktop" />
-                        <span>表单页</span>
-                    </Menu.Item>
-                    <Menu.Item key="3">
-                        <Icon type="inbox" />
-                        <span>列表页</span>
-                    </Menu.Item>
-                    <SubMenu key="sub1" title={<span><Icon type="mail" /><span>人员管理</span></span>}>
-                        <Menu.Item key="5">Option 5</Menu.Item>
-                        <Menu.Item key="6">Option 6</Menu.Item>
-                        <Menu.Item key="7">Option 7</Menu.Item>
-                        <Menu.Item key="8">Option 8</Menu.Item>
-                    </SubMenu>
-                    <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>权限管理</span></span>}>
-                        <Menu.Item key="9">Option 9</Menu.Item>
-                        <Menu.Item key="10">Option 10</Menu.Item>
-                        <SubMenu key="sub3" title="Submenu">
-                            <Menu.Item key="11">Option 11</Menu.Item>
-                            <Menu.Item key="12">Option 12</Menu.Item>
-                        </SubMenu>
-                    </SubMenu>
+                    openKeys={this.state.openKeys}
+                    onOpenChange={this.onOpenChange}>
+                    {menuConfig.map((item, key) => {
+                        if (item.children && item.children.length > 0) {
+                            return (
+                                <SubMenu key={key} title={<span><Icon type="mail" /><span>{item.title}</span></span>}>
+                                    {item.children.map((v, k) => {
+                                        return (
+                                            <Menu.Item key={'item' + key + k}>{v.title}</Menu.Item>
+                                        )
+                                    })}
+                                </SubMenu>
+                            )
+                        } else {
+                            return (
+                                <Menu.Item key={key}>
+                                    <Icon type="pie-chart" />
+                                    <span>{item.title}</span>
+                                </Menu.Item>
+                            )
+                        }
+                    })}
                 </Menu>
             </div>
         );
